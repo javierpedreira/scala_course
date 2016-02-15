c0ddd33c1328133160ad852eb072fc9c0586c2cc
Javier Pedreira <javier.pedreira@intenthq.com>
2016-02-15 14:34:44 +0000

    pascal exercise balance exercise

--- src/main/scala/Assignment1/Main.scala
+++ src/main/scala/Assignment1/Main.scala
@@ -13,12 +13,29 @@ object Main {
   /**
     * Exercise 1
     */
-  def pascal(c: Int, r: Int): Int = ????
+  def pascal(c: Int, r: Int): Int = {
+    if(c == r || c == 0) 1
+    else pascal(c - 1, r -1) + pascal(c, r -1)
+  }
 
   /**
-    * Exercise 2
+    * Exercise 2 Propperly parenthesized strings
     */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def countOpens(numOpens: Int, chars: List[Char]):Boolean =  {
+      if(chars.isEmpty)
+        numOpens == 0
+      else {
+        val n =
+          if (chars.head == '(') numOpens + 1
+          else if (chars.head == ')') numOpens - 1
+          else numOpens
+        if (n >= 0) countOpens(n, chars.tail)
+        else false
+      }
+    }
+    countOpens(0, chars)
+  }
 
   /**
     * Exercise 3
