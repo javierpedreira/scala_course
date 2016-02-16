3a2c54263409261e3ddbd0a1dcf7791c0a00746b
Javier Pedreira <javier.pedreira@intenthq.com>
2016-02-16 11:22:41 +0000

    Exercise 3

--- src/main/scala/Assignment1/Main.scala
+++ src/main/scala/Assignment1/Main.scala
@@ -40,5 +40,13 @@ object Main {
   /**
     * Exercise 3
     */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def count(m: Int, c: List[Int]) : Int = {
+      if (c.isEmpty) 0
+      else if (m - c.head == 0) 1
+      else if (m - c.head < 0) 0
+      else countChange(m - c.head, c) + countChange(m, c.tail)
+    }
+    count(money, coins.sorted)
+  }
 }
